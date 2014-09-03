612fc1ef710a02dd86f3dc6a726cd2b283ef2922
devinivy <devin@bigroomstudios.com>
2014-09-03 16:31:50 -0400

    now use dogwater 0.3.0, get model in handler.

--- arch-api/lib/index.js
+++ arch-api/lib/index.js
@@ -5,14 +5,14 @@ exports.register = function(plugin, options, next) {
     // Gotta have that dogwater
     plugin.dependency('dogwater');
     
-    // Thanks for the quotations, dogwater!
-    var Quotes = plugin.plugins['dogwater'].quotes;
-    
     // Test API route
     plugin.route({
         method: 'GET',
         path: '/random',
         handler: function (request, reply) {
+            
+            // Thanks for the quotations, dogwater!
+            var Quotes = request.model.quotes;
             
             Quotes.find()
             .then(function(quotes){
