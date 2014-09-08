cf93affac29ec8b884d1594387cd922624ce95bf
devinivy <devin@bigroomstudios.com>
2014-09-08 11:37:53 -0400

    quotes for wapid2

--- arch-api/lib/fixtures.js
+++ arch-api/lib/fixtures.js
@@ -4,52 +4,52 @@ module.exports = [{
         {
             id       : 1,
             quotation: "You eeediot!",
-            saidBy   : "Ren"
+            saidBy   : "RRen"
         },
         {
             id       : 2,
             quotation: "You're darn tootin' I'm angry! I've never been this Happy in my ENTIRE LIFE!!! Hey! I feel great! I love being angry! Thank you, Stimpy.",
-            saidBy   : "Ren"
+            saidBy   : "RRen"
         },
         {
             id       : 3,
             quotation: "And you wanna know what else? I'm gonna hitcha, and you're gonna fall... and I'm gonna look down... and <i>I'm gonna laugh</i>.",
-            saidBy   : "Ren"
+            saidBy   : "RRen"
         },
         {
             id       : 4,
             quotation: "It stays crunchy, even in milk. I may not be the President, I may not be the Pope... but as long as I have Gritty Kitty, I shall never mope.",
-            saidBy   : "Stimpy"
+            saidBy   : "SStimpy"
         },
         {
             id       : 5,
             quotation: "I want a turkey nut yogurt cane!",
-            saidBy   : "Stimpy"
+            saidBy   : "SStimpy"
         },
         {
             id       : 6,
             quotation: "You've just been visited by the Bloody Head Fairy.",
-            saidBy   : "Stimpy"
+            saidBy   : "SStimpy"
         },
         {
             id       : 7,
             quotation: "Quick, man! Cling tenaciously to my buttocks!",
-            saidBy   : "Powdered Toast Man"
+            saidBy   : "PPowdered Toast Man"
         },
         {
             id       : 8,
             quotation: "So, you whizzed on the electric fence, did ya?",
-            saidBy   : "The Devil"
+            saidBy   : "TThe Devil"
         },
         {
             id       : 9,
             quotation: "No sir, I don't like it.",
-            saidBy   : "Mr. Horse"
+            saidBy   : "MMr. Horse"
         },
         {
             id       : 10,
             quotation: "The little critters of nature... They don't know that they're ugly! That's very funny, a fly marrying a bumblebee!",
-            saidBy   : "Stinky Wizzleteats"
+            saidBy   : "SStinky Wizzleteats"
         }
     ]
 }];
